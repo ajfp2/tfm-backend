ae5480df3b08ab4e5c1c61ad360591d918ac96bb
Alfonso J <alfonsojfp@gmail.com>
2025-10-28 22:10:18 +0100

    Logout falta

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\BaseController as BaseController;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 
-class AuthController extends Controller
+
+class AuthController extends BaseController
 {
+    const MI_KEY_TOKEN = 'Api-BackendTFM_2025';
+
+    public function login3(Request $request): JsonResponse
+    {
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return $this->sendError('Credenciales incorrectas.', ['El email o la contraseña son incorrectos.'], 401);
+        }
+        $user = Auth::user();
+        if ($user->status !== 1) {
+            return $this->sendError('Acceso denegado.', ['Su cuenta no está activa. Contacte al administrador.'], 403);
+        }
+
+        $fullToken = $user->createToken(self::MI_KEY_TOKEN)->plainTextToken;
+        $token = explode('|', $fullToken)[1];
+        $expiresAt = now()->addHour();
+        $user->tokens()->latest()->first()->update([
+            'expires_at' => $expiresAt,
+        ]);
+        $data = [
+            'token' => $token,
+            'expires_at' => $expiresAt->toDateTimeString(),
+            'name' => $user->name,
+            'email' => $user->email
+        ];
+
+        return $this->sendResponseLogin($data, $token, 'Inicio de sesión exitoso.', 200);
+    }
     
     public function login(Request $request)
     {
@@ -19,12 +50,21 @@ class AuthController extends Controller
         }
 
         $user = Auth::user();
-        $token = $user->createToken('token-api')->plainTextToken;
-
-        return response()->json(['token' => $token, 'user'=> $user], 200);
+        $token = $user->createToken(self::MI_KEY_TOKEN)->plainTextToken;
+        $user['token'] = $token;
+        return $this->sendResponseLogin($user, $token, 'Inicio de sesión exitoso.', 200);
     }
 
-    public function logout(Request $request){
+    public function logout()
+    {
+        Auth::user()->tokens->each(function ($token) {
+            $token->forceDelete();
+        });
+        $response = [];
+        return $this->sendResponse($response, 'Logout exitoso.', 200);
+    }
+
+    public function logout2(Request $request){
         $user = Auth::user();
         // $user->tokens()->where('id', $tokenId)->delete();
         $user->tokens()->delete();
---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+/* TO DO  */
+// Tutorial para poner en memoria: https://blog.linuxitos.com/post/api-rest-laravel-12-sanctum-autenticacion-autorizacion-bearer
+
+class BaseController extends Controller
+{
+    /**
+     * Enviar respuesta de éxito.
+     *
+     * @param mixed  $data      Datos de la respuesta
+     * @param string $message   Mensaje a mostrar
+     * @param int    $code      Código de respuesta HTTP (por defecto 200)
+     * @return JsonResponse
+     */
+    public function sendResponseLogin($data, $token, $message = 'Conexión exitosa', $code = 200): JsonResponse
+    {
+        return response()->json([
+            'status'  => 'success',
+            'code'    => $code,
+            'message' => $message,
+            'user' => $data,
+            'token' => $token
+        ], $code);
+    }
+
+    /**
+     * Enviar respuesta de éxito.
+     *
+     * @param mixed  $data      Datos de la respuesta
+     * @param string $message   Mensaje a mostrar
+     * @param int    $code      Código de respuesta HTTP (por defecto 200)
+     * @return JsonResponse
+     */
+    public function sendResponse($data, $message = 'Conexión exitosa', $code = 200): JsonResponse
+    {
+        return response()->json([
+            'status'  => 'success',
+            'code'    => $code,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    /**
+     * Enviar respuesta de error.
+     *
+     * @param string $message        Mensaje de error
+     * @param array  $errorMessages  Errores adicionales (opcional)
+     * @param int    $code           Código de error (por defecto 400)
+     * @return JsonResponse
+     */
+    public function sendError($message, $errorMessages = [], $code = 400): JsonResponse
+    {
+        return response()->json([
+            'status'  => 'error',
+            'code'    => $code,
+            'message' => is_array($errorMessages) ? implode(' ', $errorMessages) : $message,            
+        ], $code);
+    }
+}
