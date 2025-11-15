a93c3892f62dc4f3de6b67bf02fcf38d22bc7416
Alfonso J <alfonsojfp@gmail.com>
2025-11-15 18:50:08 +0100

    Roles

---
+++ app/Models/Roles.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Roles extends Model
+{
+    use HasFactory;
+
+    protected $table = "roles";
+
+    public function user(){
+        return $this->hasMany(User::class);
+    }
+
+    /***************************
+    PARA RELACIONES DE TABLAS N:N con tabla intermedia
+    ******************************/
+
+    // use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+    // public function categories(): BelongsToMany{
+    //     return $this->belongsToMany(Category::class);
+    // }
+
+    // public function recipes(): BelongsToMany{
+    //     return $this->belongsToMany(Recipe::class);
+    // }
+
+    
+}
